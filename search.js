17d3bc3bc4b5c271333372ce9b5d795c6f592fb4
Mehdi Azabou <mehdiazabou@gmail.com>
2022-12-23 12:46:23 -0500

    add searchable tags

---
+++ search.js
@@ -0,0 +1,35 @@
+const entries = document.querySelectorAll('.entry');
+const tagButtons = document.querySelectorAll('.tag-button');
+console.log(`Buttons registered: ${tagButtons}`);
+function handleTagButtonClick(event) {
+    console.log(`Tag button clicked: ${event.target.dataset.tag}`);
+    button.style.display = 'block'
+    const clickedButton = event.target;
+    const clickedTag = clickedButton.dataset.tag;
+
+    document.body.style.backgroundColor = clickedButton.dataset.color;
+    // tagButtons.forEach(button => button.classList.add('is-light'));
+    // clickedButton.classList.remove('is-light');
+
+    entries.forEach(entry => {
+        if (entry.dataset.tags.includes(clickedTag)) {
+            entry.style.display = 'block';
+        } else {
+            entry.style.display = 'none';
+        }
+    });
+}
+tagButtons.forEach(button => button.addEventListener('click', handleTagButtonClick));
+
+const button = document.querySelector('#reset');
+
+function handleButtonClick() {
+    console.log('Reset button clicked');
+
+    document.body.style.backgroundColor = 'white';
+    entries.forEach(entry => entry.style.display = 'block');
+    button.style.display = 'none'
+
+}
+
+button.addEventListener('click', handleButtonClick);
